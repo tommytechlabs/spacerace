6bd789d6984491636b7f2ba200d2e3ce2357bf56
Tommy McHugh <tommytech1998@gmail.com>
2012-11-09 22:26:53 -0600

    First Commit

---
+++ level.js
@@ -0,0 +1,91 @@
+
+  var levelData = { 
+     1:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,1,0],
+          [0,2,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,1,0,0,0,0,0]],
+  2:     [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,0,0,0,0,1,0,0,0,0,0],
+          [0,0,0,0,0,1,0,0,2,2,2],
+          [0,0,0,0,0,1,0,0,0,1,0],
+          [0,0,0,0,0,1,0,0,0,1,0],
+          [0,0,0,0,0,1,0,0,0,1,0],
+          [0,0,0,0,0,0,0,0,0,1,0]],
+3:       [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]], 
+ 4:     [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,2,2,2,2,2,2,2,2,2,0],
+          [0,1,0,1,0,1,0,0,0,1,0]]};
+
+  var spriteData = {
+    'alien1': { sx: 0,  sy: 0,  w: 23, h: 18, cls: Alien, frames: 2 },
+    'alien2': { sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 2 },
+    'player': { sx: 0,  sy: 36, w: 26, h: 17, cls: Player },
+    'missile': { sx: 0,  sy: 86, w: 3,  h: 14, cls: Missile }
+  }
+
+  function startGame() {
+    var screen = new GameScreen("Space Race","press space to begin",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+    Game.loop();
+  }
+
+  function endGame() {
+    var screen = new GameScreen("DEATH","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+
+  function winGame() {
+    var screen = new GameScreen("Congrats, You Win!","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+  $(function() {
+    GameAudio.load({ 'fire' : 'media/laser.ogg', 'die' : 'media/explosion.ogg' }, 
+                   function() { 
+                       Game.initialize("#gameboard", levelData, spriteData,
+                                      { "start": startGame,
+                                        "die"  : endGame,
+                                        "win"  : winGame });
+                   });
+   });
+
+
